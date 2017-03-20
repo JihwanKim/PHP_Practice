9de73c77f3fbfd2924be46d1930b8e0a70639ae4
JihwanKim <jihwan6589@naver.com>
2017-03-20 23:31:38 +0900

    error to resolve. i dont know, why it did not

--- mem_queryWrite.php
+++ mem_queryWrite.php
@@ -1,14 +1,31 @@
-<meta http-equiv="Content-Type" content="text/html; charset=euc-kr"/>
 <?
+	$a = "abc";
+	$b = "n";
+	$c = "M";
+	$d = "email";
 	echo "id : $id<br>";
 	$connect = mysql_connect("localhost","root","apmsetup");
+	if (!$connect)
+		die( "mysql!".mysql_error());
 	mysql_select_db("test",$connect);
 	$sql = "insert into id_table(id,name,passwd,gender,email)";
-	$sql.= "values($id,$name,$passwd,$gender,$email."@".$address);";
-	if (mysql_query($sql))
+	// 왜이건안되지 ? 
+	/*
+		id : jh2
+		삽입 실패
+		jh2,kjh2,,,jihwan@naver.com
+		Duplicate entry '$id' for key 'PRIMARY'
+	*/
+	//$sql.= 'values("$id","$b","1234","$c.","d");';
+	$sql.= sprintf("values('%s','%s','%s','%s','%s')",$id,$name,$passwd,$gender,$email);
+	$result = mysql_query($sql);
+	if ($result)
 		echo "삽입 완료";
-	else
-		echo "삽입 실패";
+	else{
+		echo "삽입 실패<br>";
+		echo "$id,$name,$passwd,$gemder,$email@$address<br>";
+		echo mysql_error();
+	}
 	mysql_close($connect);
 
 ?>
