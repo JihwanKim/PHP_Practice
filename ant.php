2438f65193e408d8a425c6c699cba475fb6fe601
Jihwan-Kim <jihwan6589@naver.com>
2017-03-24 17:33:56 +0900

    fix antSequence

--- ant.php
+++ ant.php
@@ -1,4 +1,5 @@
 <?
+// ANT SEQUENCE
 //개미수열 15번째까지 하기
 // 처음에는 1				1
 // 다음 1 1 				2
@@ -9,41 +10,38 @@
 // 다음 1 2 2 2 1 1 3 1		8
 	$arr = array();
 	array_push($arr,1);
-	for($i = 0 ; $i < 4	; $i++){
+	for($i = 0 ; $i < 20; $i++){
 		$count = count($arr);
-		
+		// beforeValue is always 1
 		$beforeValue = 1;
+		// arr2 is new array for search new value
 		$arr2 = array();
 		
-		echo "Starting count = $count<br>";
+		//search sequence
 		for($j = 0,$sameCount = 0; ; $j++){
 			if($arr[$j] == $beforeValue){
+				// if beforeValue is same currnetValue, sameCount add 1
 				$sameCount++;
-				echo "count beforeValue = $beforeValue sameCount = $sameCount<br>";
-			}elseif($j != $count-1){
-				echo "push $beforeValue $sameCount<br>";
+			}else{
+				// if beforeValue and currentValue is not same, push beforeValue and sameCount. and then beforeValue set current value (arr[j]). and sameCount set 1.
+				array_push($arr2,$beforeValue,$sameCount);
+				$beforeValue = $arr[$j];
+				$sameCount = 1;
+			}
+			// if j is count-1 then, push beforeValue and sameCount to arr2 and then break loop
+			if ($j == $count-1){
 				array_push($arr2,$beforeValue,$sameCount);
 				break;
-			}else{
-				echo "push2 $beforeValue $sameCount<br>";
-				array_push($arr2,$beforeValue,$sameCount);
-				$beforeValue++;
 			}
 		}
-		//echo "arr2 = ".count($arr2)."<br>";
-		//$count = count($arr2);
-		//echo "arr2 = ".$count."<br>";
 		$arr = $arr2;
 		$count = count($arr);
 		echo "result count = $count<br>";
-		echo "$j arr = ";
-		/*
-		for($j = 0 ;j < $count  ; $j++){
+		echo "$jth arr = ";
+		
+		for($j = 0 ;$j < $count  ; $j++){
 			echo $arr[$j]." ";
-			echo "ver";
-		}*/
-		echo "ver";
+		}
+		echo "<br><br>";
 	}
-
-
 ?>
